b1a76c319bbac34ecdef2c8f0cbf5bfbd5832c0c
NehaSreenidhi <chagantinehasreenidhi@gmail.com>
2026-02-26 23:20:46 +0530

    Added frontend changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,26 @@
+// function App() {
+//   return (
+//     <div>
+//       <h1>Multimodal Alzheimer’s Detection</h1>
+//       <p>MRI + Clinical Data | Early Diagnosis</p>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import NewDiagnosis from "./pages/new_diagnosis";
+
 function App() {
   return (
-    <div>
-      <h1>Multimodal Alzheimer’s Detection</h1>
-      <p>MRI + Clinical Data | Early Diagnosis</p>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/new_diagnosis" element={<NewDiagnosis />} /> {/* route for new page */}
+      </Routes>
+    </Router>
   );
 }
 
--- frontend/src/pages/new_diagnosis.jsx
+++ frontend/src/pages/new_diagnosis.jsx
@@ -1,12 +1,10 @@
-import React from "react";
-
-const Home = () => {
+const NewDiagnosis = () => {
   return (
     <div style={{ padding: "2rem" }}>
-      <h1>Welcome to the New Page!</h1>
+      <h1>Welcome to the New Diagnosis!</h1>
       <p>This is a new page added to your Alzheimer’s Detection project frontend.</p>
     </div>
   );
 };
 
-export default Home;
+export default NewDiagnosis;
