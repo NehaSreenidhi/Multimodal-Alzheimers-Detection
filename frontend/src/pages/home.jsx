c704fc29ae80a0539c246cbfc678ca4b8ebc223f
NehaSreenidhi <chagantinehasreenidhi@gmail.com>
2026-02-26 22:54:36 +0530

    home page added

---
+++ frontend/src/pages/home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NewPage = () => {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Welcome to the New Page!</h1>
+      <p>This is a new page added to your Alzheimer’s Detection project frontend.</p>
+    </div>
+  );
+};
+
+export default NewPage;
